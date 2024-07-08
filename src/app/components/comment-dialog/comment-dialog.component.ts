54f33796c6ffd0059eb96b2bf47c6e3e42d76255
ojou-less <mark.fischer0404@gmail.com>
2024-07-08 16:09:43 +0200

    was geht lo gggggggg

---
+++ src/app/components/comment-dialog/comment-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comment-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './comment-dialog.component.html',
+  styleUrl: './comment-dialog.component.css'
+})
+export class CommentDialogComponent {
+
+}
